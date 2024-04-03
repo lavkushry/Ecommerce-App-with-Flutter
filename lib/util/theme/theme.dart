73973ddf868bacdabd3a46dd670be084cce88afd
lavkushry <lavkushry@gmail.com>
2024-04-03 15:05:03 +0530

    Added app bar

--- lib/util/theme/theme.dart
+++ lib/util/theme/theme.dart
@@ -1,9 +1,14 @@
 
+import 'package:ecom_app/util/theme/custom_themes/appbar_theme.dart';
 import 'package:ecom_app/util/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 
+import 'custom_themes/elevated_button_theme.dart';
+
 class TAppTheme{
+
   TAppTheme._();
+
   static ThemeData lightTheme= ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -11,8 +16,11 @@ class TAppTheme{
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: TTextTheme.lightTextTheme,
-
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme
   );
+
+
   static ThemeData darkTheme= ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -20,6 +28,8 @@ class TAppTheme{
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+      appBarTheme: TAppBarTheme.darkAppBarTheme
   );
 
 }
