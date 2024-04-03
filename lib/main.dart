ee07487d286ec029c1d7160ec91187596a2f272c
lavkushry <lavkushry@gmail.com>
2024-04-03 16:33:55 +0530

    Added Conatants like colors.dart and sizes.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
-import 'package:ecom_app/util/theme/theme.dart';
+import 'package:ecom_app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  //Todo: Add Widgit Binding
+  //Todo: init Local Storage
+  //Todo: Await Native Splash
+  //Todo: Initialize FireBase
+  //Todo: Initialize Authentication
+
   runApp(const App());
 }
-class App extends StatelessWidget {
-  const App({super.key});
+/// Use this class to Setup Cusotm THeme, Inital Binding, and Any Animations
 
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme
-    );
-  }
-}
