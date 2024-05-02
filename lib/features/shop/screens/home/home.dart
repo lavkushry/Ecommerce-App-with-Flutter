378b938057baa1e36fee9e67363b7a9550eaa0aa
lavkushry <lavkushry@gmail.com>
2024-05-02 17:26:52 +0530

    Added Catrgories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,12 @@
 import 'package:ecom_app/common/widgets/appbar/appbar.dart';
 import 'package:ecom_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:ecom_app/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:ecom_app/common/widgets/image_text_widgets/vertical_image_text.dart';
 import 'package:ecom_app/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:ecom_app/common/widgets/texts/section_heading.dart';
 import 'package:ecom_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:ecom_app/util/constants/colors.dart';
+import 'package:ecom_app/util/constants/image_strings.dart';
 import 'package:ecom_app/util/constants/sizes.dart';
 import 'package:ecom_app/util/constants/text_strings.dart';
 import 'package:ecom_app/util/device%20/device_utility.dart';
@@ -12,6 +15,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
+
+import 'widgets/home_categories.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -33,9 +38,29 @@ class HomeScreen extends StatelessWidget {
                   ),
 
                   /// Search Bar
-                  TSearchContainer(
+                  const TSearchContainer(
                     text: 'Search in Store',
-                  )
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// Categories Container
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        ///  -- Heading
+                        const TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: TColors.white,
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwItems),
+
+                        /// -- Categories
+                        THomeCategories()
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
